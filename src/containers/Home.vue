2d0e29e0873d286cf541c44e3f7fa4f879596de4
Aler Denisov <aler.zampillo@gmail.com>
2017-12-28 15:15:24 +0300

    removed mocks

--- src/containers/Home.vue
+++ src/containers/Home.vue
@@ -20,11 +20,32 @@ export default {
   },
   data () {
     // mock field
+    function randomGenes () {
+      let result = {}
+      let genetypes = ['nature', 'water', 'beast', 'chaos', 'toxic']
+      let count = 1 + Math.floor(Math.random() * 2)
+      while (count > 0) {
+        const geneIndex = Math.floor(Math.random() * genetypes.length)
+        const gene = genetypes[geneIndex]
+        genetypes = genetypes.splice(geneIndex, 1)
+
+        result[gene] = 20 + Math.floor(Math.random() * 30)
+        count--
+      }
+
+      return result
+    }
+
     function randomCard () {
       return {
         title: 'Prynoth',
         picture: `/static/images/monsters/card-common0${('00000' + Math.floor(Math.random() * 100)).slice(-3)}.png`,
-        active: false
+        active: false,
+        attributes: {
+          attack: 5 + Math.floor(Math.random() * 10),
+          health: 5 + Math.floor(Math.random() * 10)
+        },
+        genes: randomGenes()
       }
     }
 
