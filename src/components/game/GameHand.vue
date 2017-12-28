72d6ef16e7d53bd3566d37fdfa0695a04ec68143
Aler Denisov <aler.zampillo@gmail.com>
2017-12-28 06:30:24 +0300

    Rotation calculation

--- src/components/game/GameHand.vue
+++ src/components/game/GameHand.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   div(:class="b(modifiers)")
-    div(:class="b('handle', modifier)" v-for='card in hand')
+    div(:class="b('handle', modifiers)" v-for='card, index in hand' :ref='`game-hand__handle-${index}`')
       game-card(:title='card.title' :height='2' size='medium' :quality='card.quality' :picture='card.picture' :class="b('card', modifiers)")
 </template>
 
@@ -22,6 +22,14 @@ export default {
         side
       }
     }
+  },
+
+  mounted () {
+    const cardRefs = Object.keys(this.$refs).filter(ref => ref.startsWith('game-hand__handle-'))
+    const step = Math.min(10, 30 / (cardRefs.length - 1))
+    cardRefs.forEach((cardRef, index) => {
+      this.$refs[cardRef][0].style.transform = `rotate(${step * index}deg)`
+    })
   }
 }
 </script>
@@ -35,12 +43,12 @@ export default {
     padding-bottom: 300px;
     transform-origin: center bottom;
 
-    $elements: 10;
-    @for $i from 0 to $elements {
-      &:nth-child(#{$i}) {
-        transform: rotate(5deg * $i);
-      }
-    }
+    // $elements: 10;
+    // @for $i from 0 to $elements {
+    //   &:nth-child(#{$i}) {
+    //     transform: rotate(5deg * $i);
+    //   }
+    // }
   }
 }
 </style>
