92334abb15bdefe51ee2a6b918f0924a2e3cea15
Aler Denisov <aler.zampillo@gmail.com>
2017-12-28 04:03:50 +0300

    Base view

---
+++ src/components/game/GameHand.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+  div(:class="b(modifiers)")
+    div(:class="b('handle', modifier)" v-for='card in hand')
+      game-card(:title='card.title' :height='2' size='medium' :quality='card.quality' :picture='card.picture' :class="b('card', modifiers)")
+</template>
+
+<script>
+import GameCard from '@/components/game/GameCard.vue'
+
+export default {
+  name: 'game-hand',
+  props: ['hand', 'side'],
+
+  components: {
+    GameCard
+  },
+
+  computed: {
+    modifiers () {
+      const { side } = this
+      return {
+        side
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.game-hand {
+  position: absolute;
+
+  &__handle {
+    position: absolute;
+    padding-bottom: 300px;
+    transform-origin: center bottom;
+
+    $elements: 10;
+    @for $i from 0 to $elements {
+      &:nth-child(#{$i}) {
+        transform: rotate(5deg * $i);
+      }
+    }
+  }
+}
+</style>
